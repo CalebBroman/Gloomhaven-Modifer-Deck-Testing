7452b782492304961ada75e684c35f113e0c704e
Caleb Broman <caleb.broman@gmail.com>
2026-01-10 14:16:51 -0600

    Finished the attack comparison function; updated and fixed previous functions in the script.

--- scripts/attackcomparisons.R
+++ scripts/attackcomparisons.R
@@ -5,23 +5,23 @@ target <- list(health = 7, shield = 1, retaliate = 0, poison = F, wound = F,
 
 exampleAttack1 <- list(roll = F, shuffle = F, damage = 6, 
                        push = 0, pull = 0, pierce = 2, target = 1, poison = F, 
-                       wound = F, immobilize = F, disarm = F, stun = F, muddle = F, curse = F)
+                       wound = F, immobilize = F, disarm = F, stun = T, muddle = F, curse = F)
 
-exampleAttack2 <- list(roll = F, shuffle = F, damage = 3, 
+exampleAttack2 <- list(roll = F, shuffle = F, damage = 8, 
                        push = 0, pull = 0, pierce = 0, target = 1, poison = F, 
                        wound = F, immobilize = F, disarm = F, stun = F, muddle = F, curse = F)
 
 #calculates the state of the target after the attack
 #all negative conditions are set to 1 if the target dies
 targetAttacked <- function(attack, target) {
-  if (attack$pierce > target$shield) {
+  if (attack$pierce >= target$shield) {
     actualDamage <- attack$damage
   } else {
-    actualDamage <- attack$damage + attack$pierce - attack$shield
-    if (actualDamage < 0) actualDamage <- 0
+    actualDamage <- attack$damage + attack$pierce - target$shield
+    if (actualDamage < 0) {actualDamage <- 0}
   }
+  if (actualDamage > target$health) {actualDamage <- target$health}
   health <- target$health - actualDamage
-  if (health < 0) health <- 0
   list(health = health, damage = actualDamage,
        push = attack$push, pull = attack$pull, 
        target = attack$target, poison = target$poison || attack$poison || !health, 
@@ -39,7 +39,6 @@ printAttackEffects <- function(enemyState) {
   if (enemyState$health) {
     if (enemyState$push != 0) {print(str_c("push: ", enemyState$push))}
     if (enemyState$pull != 0) {print(str_c("pull: ", enemyState$pull))}
-    if (enemyState$pierce != 0) {print(str_c("pierce: ", enemyState$pierce))}
     if (enemyState$poison == T) {print("poison")}
     if (enemyState$wound == T) {print("wound")}
     if (enemyState$immobilize == T) {print("immobilize")}
@@ -51,9 +50,16 @@ printAttackEffects <- function(enemyState) {
 }
 
 #compares two attacks, returns 1 if the first is better, 
-#   2 if the second is better, and -1 if ambiguous
+#   2 if the second is better, 0 if equivalent, and -1 if ambiguous
 attackComparison <- function(attack1, attack2, target) {
-  
+  eState1 <- as.numeric(targetAttacked(attack1, target))
+  eState2 <- as.numeric(targetAttacked(attack2, target))
+  dif <- (eState1-eState2)[2:length(eState1)]
+  print(dif)
+  if (sum(dif != 0) == 0) return(0)
+  else if (sum(dif < 0) == 0) return(1)
+  else if (sum(dif > 0) == 0) return(2)
+  else return(-1)
 }
 
 
